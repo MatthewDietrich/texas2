a864bd0fac2de0100aade91fb437723bd78045b2
Matt Dietrich <mbd321123@gmail.com>
2026-06-17 09:10:21 -0500

    fit footer link on about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: "app-about",
   standalone: true,
-  imports: [NavBarComponent, FooterComponent],
+  imports: [RouterLink, NavBarComponent, FooterComponent],
   templateUrl: "./about.component.html",
   styles: [
     `
